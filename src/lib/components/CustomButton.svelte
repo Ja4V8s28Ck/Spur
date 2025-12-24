37693b08a9e83b08391d836446bf61b287d75c24
Ja4V8s28Ck <66721411+Ja4V8s28Ck@users.noreply.github.com>
2025-12-24 18:34:05 +0530

    feat: create a custom button with loading functionalities

---
+++ src/lib/components/CustomButton.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+  import type { Component, ComponentProps } from "svelte";
+  import { LoaderCircle } from "@lucide/svelte";
+
+  type Props = ComponentProps<Button> & {
+    icon?: Component;
+    loading?: boolean;
+  };
+
+  let {
+    icon: Icon,
+    loading = false,
+    disabled = false,
+    children,
+    class: className = "",
+    ...restProps
+  }: Props = $props();
+</script>
+
+<Button
+  disabled={disabled || loading}
+  class="cursor-pointer {className}"
+  {...restProps}
+>
+  {@render children?.()}
+  {#if loading}
+    <LoaderCircle class="animate-spin" />
+  {:else if Icon}
+    <Icon this={Icon} />
+  {/if}
+</Button>
